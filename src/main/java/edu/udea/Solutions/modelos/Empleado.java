38565108107b6e3791c7b76e8e5b810e479bf4a4
Diegomartineztic <dalejandronmc@hotmail.com>
2022-08-25 12:18:42 -0500

    Diego empleado

--- src/main/java/edu/udea/Solutions/modelos/Empleado.java
+++ src/main/java/edu/udea/Solutions/modelos/Empleado.java
@@ -1,4 +1,31 @@
 package edu.udea.Solutions.modelos;
 
 public class Empleado {
+    long id;
+    string email;
+    updatedAt
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public string getEmail() {
+        return email;
+    }
+
+    public void setEmail(string email) {
+        this.email = email;
+    }
+
+    public Empleado(long id, string email) {
+        this.id = id;
+        this.email = email;
+    }
+
+
+
 }
