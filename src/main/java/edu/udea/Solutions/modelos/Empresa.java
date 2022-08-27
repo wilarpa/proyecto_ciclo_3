6576d421235824b799f36096cd34d50c5de59cf1
ander5360 <anderson_santamaria53@hotmail.com>
2022-08-26 19:16:05 -0500

    se modela clase empresa

--- src/main/java/edu/udea/Solutions/modelos/Empresa.java
+++ src/main/java/edu/udea/Solutions/modelos/Empresa.java
@@ -1,4 +1,21 @@
 package edu.udea.Solutions.modelos;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String NIT;
+
+
 }
