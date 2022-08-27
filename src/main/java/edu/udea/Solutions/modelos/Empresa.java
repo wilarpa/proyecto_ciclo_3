97240acbafa1c0d86b5fc16a4ad040c9c3f62874
ander5360 <anderson_santamaria53@hotmail.com>
2022-08-26 19:32:11 -0500

    se generan setters y getters

--- src/main/java/edu/udea/Solutions/modelos/Empresa.java
+++ src/main/java/edu/udea/Solutions/modelos/Empresa.java
@@ -1,12 +1,10 @@
 package edu.udea.Solutions.modelos;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Empresa")
 public class Empresa {
 
     @Id
@@ -18,4 +16,53 @@ public class Empresa {
     private String NIT;
 
 
+    public Empresa() {
+    }
+
+    public Empresa(String nombre, String direccion, String telefono, String NIT) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.NIT = NIT;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(String NIT) {
+        this.NIT = NIT;
+    }
 }
