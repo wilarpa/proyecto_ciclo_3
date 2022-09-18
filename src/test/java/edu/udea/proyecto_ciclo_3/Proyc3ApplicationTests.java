e96b7ef4d23e5ff8cb9e93ffeb13cc499e40c8e7
juan lasso <ingjclasso@gmail.com>
2022-09-18 16:32:03 -0500

    actualizacion

---
+++ src/test/java/edu/udea/proyecto_ciclo_3/Proyc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.UdeA.proyecto_ciclo_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
